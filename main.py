d1eb3e65de09c19df4c499e4ff10fdde2d9476cc
Carlos Outeiral <couteiral@gmail.com>
2017-06-12 17:53:08 +0100

    Create main.py

--- main.py
+++ main.py
@@ -3,4 +3,19 @@ import math
 import mathutils
 import sys
 import mendeleev
+import draw
+import irc
+import functions
 
+def plotMolecule(Molecule, output):
+  
+  draw.initialize()
+  draw.drawAtoms(Molecule)
+  draw.drawBonds(Molecule)
+  bpy.ops.wm.save_as_mainfile(output)
+  
+  return None
+
+# Plot a molecule
+HCN = functions.getMolecule('molecule.dat', 3)
+plotMolecule(HCN, 'molecule.blend')
